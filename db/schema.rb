7dfc1740c98f8c55d5e2776977730f991c4ff7f2
Joao Felipe <joaofelipe1294@gmail.com>
2021-01-17 20:54:15 -0300

    Fixed model user.

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_01_17_232504) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
