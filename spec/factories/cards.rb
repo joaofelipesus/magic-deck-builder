949ec250c6152bc63f942ed65995f08792eee231
Joao Felipe <joaofelipe1294@gmail.com>
2021-01-26 22:59:34 -0300

    Added model card

---
+++ spec/factories/cards.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :card do
+    name { 'Akroma, Angel of Wrath' }
+    external_id { '75703f17-9440-5c28-87e6-c2c7d41dbaa1' }
+    types { 'Angel' }
+    mana_cost { '5{W}{W}{W}' }
+    power { 6 }
+    toughenss { 6 }
+    colors { 'white' }
+  end
+end
